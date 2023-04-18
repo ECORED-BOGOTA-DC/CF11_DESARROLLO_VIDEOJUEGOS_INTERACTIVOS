9a7ae35018624d46a592d79fd25dc1dc2130a3a9
jorozcop <maechavarriaor@gmail.com>
2023-04-18 11:05:45 -0500

    tema 3
    tema 3

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -19,10 +19,72 @@
       p De esta manera se subsanan errores que aparecían al producirse excepciones en el código. Para llevar a cabo estas pruebas se ha hecho uso de una funcionalidad implementada por Unity llamada Test Runner que permite su configuración y ejecución. 
 
 
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        .bloque-texto-g.color-secundario.p-3.p-sm-4.p-md-4
+          .bloque-texto-g1__img(
+            :style="{'background-image': `url(${require('@/assets/curso/temas/tema3/img2.png')})`}"
+          )
+          .bloque-texto-g1__texto.p-3
+            p.mb-4 Algo que la mayoría de las veces solemos dejar completamente de lado a la hora de desarrollar nuevas features en nuestros juegos son, sin duda, los Unit Tests.
+            p El Unity Test Runner hace uso de la #[b librería NUnit que es una librería de pruebas de software libre para lenguajes .Net.] El Test Runner permite ejecutar los test tanto en modo editor (edit mode) como en modo de juego (play mode). 
+
+    .row.mb-5
+      p Sin embargo, el uso de estas herramientas que proporciona el software de Unity 3D se deja casi siempre de lado ya sea por poco conocimiento sobre el uso de este framework, ya que este posee muchas bondades. De esta manera cabe resaltar que la implementación de estos test en nuestros proyectos siempre nos va a demandar un buen gasto de tiempo y esfuerzo extra que no siempre podemos o estamos dispuestos a perder, sumado a la falsa sensación de pérdida de tiempo por estar invirtiendo una parte importante de nuestro trabajo en desarrollar código que ni siquiera pertenece al producto final, puede que el hacer un repaso de algunos de sus pros y contras nos haga verlo de otro modo. Al respecto:
+
+    .row.mb-5
+      .col-lg-6
+        .tarjeta-morada.p-4
+          .row.justify-content-center
+            .col-6.col-lg-3.mb-4
+              figure
+                img(src='@/assets/curso/temas/tema3/img3.svg', alt='Texto que describa la imagen').animacion-img-3
+
+          h4.text-center.texto-blanco Ventajas 
+
+          ul.lista-ul--color
+            li
+              i.fas.fa-plus.color-vineta1
+              span.texto-blanco
+                | Aplicar test en nuestro código nos da seguridad y confianza en el comportamiento esperado de las lógicas que implementamos.]
+            li
+              i.fas.fa-plus.color-vineta1
+              span.texto-blanco
+                | Algo muy importante es que sirve como documentación para nuevos desarrolladores que trabajen en tu código.
+            li
+              i.fas.fa-plus.color-vineta1
+              span.texto-blanco
+                | Nos obliga a implementar las cosas enfocándonos siempre a su testeo.
+            li
+              i.fas.fa-plus.color-vineta1
+              span.texto-blanco
+                | Nos ayuda a anticipar y localizar posibles bugs de manera más rápida y eficiente.
+            li
+              i.fas.fa-plus.color-vineta1
+              span.texto-blanco
+                | Nos convierte en mejores profesionales.
 
 
 
+      .col-lg-6
+        .tarjeta-azul.p-4
+          .row.justify-content-center
+            .col-6.col-lg-3.mb-4
+              figure
+                img(src='@/assets/curso/temas/tema3/img3.svg', alt='Texto que describa la imagen').animacion-img-3
 
+            h4.text-center Desventajas 
+
+          ul.lista-ul--color
+            li
+              i.fas.fa-times.color-vineta2
+              | Inversión de tiempo extra de desarrollo (a veces puede llegar a ser demasiado).
+            li
+              i.fas.fa-times.color-vineta2
+              | No siempre es fácil testear todo (por ejemplo, comportamientos de UI).
+            li
+              i.fas.fa-times.color-vineta2
+              | No implementarlos de manera adecuada puede crearnos una falsa seguridad que puede ser peligrosa.
 
 
 
