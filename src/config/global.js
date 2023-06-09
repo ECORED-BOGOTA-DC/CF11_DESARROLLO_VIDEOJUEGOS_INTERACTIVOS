bb32ff7e6d28822752f349aa5bd360f9495b3e53
payepesch <maechavarriaor@gmail.com>
2023-06-08 19:32:44 -0500

    Ajuste

--- src/config/global.js
+++ src/config/global.js
@@ -148,7 +148,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/CFA_11_ V1_228108.pdf',
       },
       {
         icono: 'fas fa-download',
